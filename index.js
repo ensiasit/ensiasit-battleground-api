ac54c74d2cdc43c061a81fb0a9ed52ff4d84513a
El Mehdi ASSALI <eassali@ingeniance.com>
2020-07-31 12:16:38 +0200

    Test docker build

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Version 0.1.0"));
 
 const port = process.env.SERVER_PORT || 80;
 app.listen(port, () => console.log(`Listening on port ${port}`));
