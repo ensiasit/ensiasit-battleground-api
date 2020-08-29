a1cffc25a30eca4ffca6213c74ed141d387e3d0f
protozeit <sjlooaud@gmail.com>
2020-08-29 17:10:16 +0100

    add index.js for clarification tests

---
+++ test/clarifications/index.js
@@ -0,0 +1,12 @@
+const postTest = require("./post");
+// const putTest = require("./put");
+// const getTest = require("./get");
+// const deleteTest = require("./delete");
+
+const clarification = {
+  username: "protozeit",
+  problem_slug: "messenger-group-naming-scheme",
+  text: "Omae wa mou shindeiru",
+};
+
+postTest(clarification);
